f4e1588eee410f71e4bb665bd02a5f53edcc2735
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-05-19 22:06:46 -0700

    deployed

---
+++ main.js
@@ -0,0 +1,989 @@
+const janData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const febData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const marData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const aprData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const mayData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const junData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const julData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const augData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const sepData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const octData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const novData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+const decData = [
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },
+    {
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },{
+        date: "1 Jan 2024",
+        count: 0
+    },
+]
+
+// let sum = 0;
+// function sumOfGallons(month){
+//     monthlySummary.data.forEach(d => {
+//         sum += d.count;
+//     })
+//     console.log(month);
+//     return sum;
+// }
+
+const monthlySummary = [
+    {
+        month: "January",
+        counts: 0,
+        data: [...janData]
+    },
+    {
+        month: "February",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "March",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "April",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "May",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "June",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "July",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "August",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "September",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "October",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "November",
+        counts: 0,
+        data: [...febData]
+    },
+    {
+        month: "December",
+        counts: 0,
+        data: [...febData]
+    }
+]
+
+
+
+monthlySummary.forEach((monthData, i) => {
+    document.querySelector('.container').innerHTML += `
+    <div class="accordin">
+        <header class="accordin-title">
+            <em>Month</em>
+            <h3>${monthData.month}</h3>
+            <p>Monthly Summary: ${monthData.counts} gallons</p>
+            <i class="accordin-icon ph-bold ph-caret-down"></i>
+        </header>
+        <section></section>
+    </div>
+    `;
+})
+
+
+
+document.querySelectorAll('.accordin').forEach((acc,i)  => {
+    monthlySummary[i].data.forEach(d =>{
+        acc.lastElementChild.innerHTML += `
+        <div class="box ${monthlySummary[i].month}">
+            <date>${d.date}</date>
+            <i>${d.count}</i>
+        </div>
+        `;
+    })
+})
+let accordinHeaders  = document.querySelectorAll('.accordin header');
+
+accordinHeaders.forEach((head, i)  => {
+    head.addEventListener('click', ()  => {
+        document.querySelectorAll('.accordin-icon')[i].classList.toggle('active');
+        if(document.querySelectorAll('.accordin-icon')[i].classList.contains('active')){
+            document.querySelectorAll('.box.' + monthlySummary[i].month).forEach(box => box.classList.add('show'));
+        } else {
+            document.querySelectorAll('.box.' + monthlySummary[i].month).forEach(box => box.classList.remove('show'));
+        }
+    })
+})
